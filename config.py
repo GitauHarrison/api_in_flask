6dc80c7ec70697d383c8cd945174407171e9d441
harry <harrisongitau3@gmail.com>
2023-06-22 11:19:21 +0300

    Dev: Define needed application configurations

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    """All application configurations"""
+
+    # Web form security
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-cannot-guess'
+
+    # Database configurations
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Email
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+    ADMINS = ['ADMINS']
+
+    # Twilio Verify
+    TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+    TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+    TWILIO_VERIFY_SERVICE_ID = os.environ.get('TWILIO_VERIFY_SERVICE_ID')
+
+    # Deployment
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
