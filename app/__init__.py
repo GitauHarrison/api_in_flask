30ab4ab1a0ca1cea6a11bdbe31d715e055029727
harry <harrisongitau3@gmail.com>
2023-10-13 02:49:09 +0300

    Dev: Create errors blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -33,6 +33,9 @@ def create_app(config_class=Config):
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
 
     return app
 
