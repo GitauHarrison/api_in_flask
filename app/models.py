b813a652bc267807fe108953470d57ce85a78841
harry <harrisongitau3@gmail.com>
2023-10-14 08:43:47 +0300

    Dev: Add pagination to collection of user and post representations
    - Define a generic mixin class used to provide extra functionality to User and Post classes
    - Inherit the mixin class in User and Post classes
    
    The mixin class returns a list of resources (user resources or post resources)
    We ensure that we provide as much info about the pagination controls to the client as metadata
    We also return links that take a client to another collection in another page

--- app/models.py
+++ app/models.py
@@ -3,6 +3,30 @@ from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
+from flask import url_for
+
+
+class PaginatedAPIMixin(object):
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = query.paginate(page=page, per_page=per_page, error_out=False)
+        data = {
+            'items': [item.to_dict() for item in resources.items],
+            '_meta': {
+                'page': page,
+                'per_page': per_page,
+                'total_pages': resources.pages,
+                'total_items': resources.total
+            },
+            '_links': {
+                'self': url_for(endpoint, page=page, per_page=per_page, **kwargs),
+                'next': url_for(endpoint, page=page + 1, per_page=per_page, **kwargs) \
+                    if resources.has_next else None,
+                'prev': url_for(endpoint, page=page - 1, per_page=per_page, **kwargs) \
+                    if resources.has_prev else None
+            }
+        }
+        return data
 
 
 @login.user_loader
@@ -10,7 +34,7 @@ def load_user(id):
     return User.query.get(int(id))
 
 
-class User(UserMixin, db.Model):
+class User(PaginatedAPIMixin, UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -33,9 +57,31 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
+    def to_dict(self, include_email=False):
+        data = {
+            'id': self.id,
+            'username': self.username,
+            'last_seen': self.last_seen.isoformat() + 'Z',
+            'about_me': self.about_me,
+            'post_count': self.posts.count(),
+            '_links': {
+                'self': url_for('api.get_user', id=self.id),
+                'avatar': self.avatar(128)
+            }
+        }
+        if include_email:
+            data['email'] = self.email
+        return data
+
+    def from_dict(self, data, new_user=False):
+        for field in ['username', 'email', 'about_me']:
+            if field in data:
+                setattr(self, field, data[field])
+            if new_user and 'password' in data:
+                self.set_password(data['password'])
 
 
-class Post(db.Model):
+class Post(PaginatedAPIMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64))
     body = db.Column(db.String(140))
@@ -44,3 +90,28 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'Post: {self.body}'
+
+    # From object to Python representation
+    def to_dict(self):
+        data = {
+            "id": self.id,
+            "title": self.title,
+            "body": self.body,
+            "timestamp": self.timestamp.isoformat() + 'Z',
+            "author": {
+                "id": self.author.id,
+                "username": self.author.username,
+                "about_author": self.author.about_me
+            },
+            "_links": {
+                "to_this_post": url_for('api.get_post', id=self.id),
+                "to_post_author": url_for('api.get_user', username=self.author.username)
+            }
+        }
+        return data
+
+    # From Python representation to object
+    def from_dict(self, data):
+        for field in ["tite", "body", "author_id"]:
+            if field in data:
+                setattr(self, field, data[field])
