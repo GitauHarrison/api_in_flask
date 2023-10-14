6d7f03ceada31c8b89cdb0af466c02d62734d192
harry <harrisongitau3@gmail.com>
2023-10-15 02:37:27 +0300

    Dev: Handle API errors
    - Add a dedicated method that handles the 400 Bad Request error common in APIs
    
    - Return a Response() object from Flask with the error payload

--- app/api/errors.py
+++ app/api/errors.py
@@ -1,2 +1,16 @@
-def bad_request():
-    pass
+from flask import jsonify
+from werkzeug.http import HTTP_STATUS_CODES
+
+
+def bad_request(message):
+    '''Common API error: 400 Bad Request (invalid data used in request)'''
+    return error_response(400, message)
+
+
+def error_response(status_code, message=None):
+    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
+    if message:
+        payload['message'] = message
+    response = jsonify(payload)
+    response.status_code = status_code
+    return response
